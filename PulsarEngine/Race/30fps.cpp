4e22da1be86abb7b8f9e7d183bf8fd1798bf9a85
VanzaKart Wii <andrea.sinesi20@gmail.com>
2026-05-26 16:58:40 +0200

    Added 30fps patch (thx Weebo)

---
+++ PulsarEngine/Race/30fps.cpp
@@ -0,0 +1,37 @@
+#include <kamek.hpp>
+#include <Settings/Settings.hpp>
+#include <MarioKartWii/Race/RaceData.hpp>
+#include <runtimeWrite.hpp>
+
+namespace Pulsar {
+namespace Race {
+
+kmWrite32(0x80554224, 0x3C808000);
+kmWrite32(0x80554228, 0x88841200);
+kmWrite32(0x8055422C, 0x48000044);
+
+kmRuntimeUse(0x80001200);
+
+void ApplyFPSSetting() {
+    const Racedata* racedata = Racedata::sInstance;
+    if(racedata == nullptr) return;
+    
+    const GameMode mode = racedata->racesScenario.settings.gamemode;
+    
+    // always 60 fps in time trial
+    if(mode == MODE_TIME_TRIAL || mode == MODE_GHOST_RACE) {
+        *reinterpret_cast<u8*>(kmRuntimeAddr(0x80001200)) = 0;
+        return;
+    }
+    
+    const Settings::Mgr& settings = Settings::Mgr::Get();
+    u8 fpsSetting = settings.GetSettingValue(Settings::SETTINGSTYPE_RACE1, SETTINGRACE1_RADIO_FPS);
+    
+    *reinterpret_cast<u8*>(kmRuntimeAddr(0x80001200)) = (fpsSetting == MENUSETTING_FPS_30) ? 1 : 0;
+}
+
+static SectionLoadHook ApplyFPS(ApplyFPSSetting);
+static RaceLoadHook ApplyFPSRace(ApplyFPSSetting);
+
+} //namespace Race
+} //namespace Pulsar
