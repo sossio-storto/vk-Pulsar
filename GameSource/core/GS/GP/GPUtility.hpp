0b45e43ffdf961484eeeacf34c96fcc12a772dc3
VanzaKart Wii <andrea.sinesi20@gmail.com>
2026-05-27 22:45:10 +0200

    Varius Fix

--- GameSource/core/GS/GP/GPUtility.hpp
+++ GameSource/core/GS/GP/GPUtility.hpp
@@ -1,12 +1,14 @@
 #ifndef _GPUTILITY_
 #define _GPUTILITY_
 #include <types.hpp>
+#include <core/GS/GP/GPEnum.hpp>
 #include <core/GS/GP/GPTypes.hpp>
 
 namespace GP {
 
-void strzcpy(char* dest, const char* src, u32 len); //80108e78 length of buffer, including space for '\0'
-}//namespace GP
+void strzcpy(char* dest, const char* src, u32 len);  // 80108e78 length of buffer, including space for '\0'
 
+GP::Result gpiAppendStringToBuffer(Connection** connection, IBuffer* outputBuffer, const char* buffer);
 
+}  // namespace GP
 #endif
